a81726b787d190a0868b4ff5cb377f0aaa367d52
edgrams <edgramswork@gmail.com>
2016-12-17 13:44:50 -0600

    Chapter 2 - reducer/main & jar

---
+++ hadoop/src/main/java/Chapter2/MaxTemperatureReducer.java
@@ -0,0 +1,24 @@
+package Chapter2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Created by edgrams on 12/16/16.
+ */
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
+    }
+}
