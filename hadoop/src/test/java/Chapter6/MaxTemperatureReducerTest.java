c3923c7a5122933a2dd87bfdc633a5b649238af4
edgrams <edgramswork@gmail.com>
2017-01-06 14:51:34 -0600

    reducer tests

---
+++ hadoop/src/test/java/Chapter6/MaxTemperatureReducerTest.java
@@ -0,0 +1,25 @@
+package Chapter6;
+
+import Chapter2.MaxTemperatureReducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by edgrams on 1/6/17.
+ */
+public class MaxTemperatureReducerTest {
+    @Test
+    public void returnsMaximumIntegerInValues() throws IOException, InterruptedException {
+        new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+                .withReducer(new MaxTemperatureReducer())
+                .withInput(new Text("1950"),
+                        Arrays.asList(new IntWritable(10), new IntWritable(5)))
+                .withOutput(new Text("1950"), new IntWritable(10))
+                .runTest();
+    }
+}
